5da53d0c521090eb95e87df90123fce7bf7dfee9
root <root@DESKTOP-D4RTLKG.localdomain>
2023-05-06 01:25:26 -0400

    test

--- cli.js
+++ cli.js
@@ -10,10 +10,11 @@
 
 import minimist from 'minimist'; 
 import moment from 'moment-timezone'; 
+import fetch from 'node-fetch';
 
 const miniArgs = minimist(process.argv.slice(2)); 
-const latitude = miniArgs.n || (0-miniArgs.s);
-const longitude = miniArgs.e || (0-miniArgs.w);
+var latitude = args.n || -1 * args.s;
+var longitude = args.e || -1 * args.w;
 const timezone = moment.tz.guess();
 const helpText = "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n\t-h \t\tShow this help message and exit.\n\t-n, -s\t\tLatitude: N positive; S negative.\n\t-e, -w\t\tLongitude: E positive; W negative.\n\t-z\t\tTime zone: uses tz.guess() from moment-timezone by default.\n\t-d 0-6\t\tDay to retrieve weather: 0 is today; defaults to 1.\n\t-j\t\tEcho pretty JSON from open-meteo API and exit.\n"; 
 if (miniArgs.h) {
@@ -24,7 +25,6 @@ if (miniArgs.h) {
 
 // Make a request
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&temperature_unit=fahrenheit&timezone=' + timezone);
-
 // Get the data from the request
 const data = await response.json();
 
@@ -32,6 +32,10 @@ const days = miniArgs.d;
 const weather = data.daily.precipitation_hours[days]; 
 const output = ""; 
 
+if (args.j){
+    console.log(data);
+    process.exit(0);
+}
 
 if (days == 0) {
   console.log("today.")
